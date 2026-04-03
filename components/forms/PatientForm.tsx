f45c81536a8e7ec5768c3834d790a4637559c6b5
Mitchellnkem <Mitchellnkemgmailcomgit config --global user.email Mitchellnkemgmail.com>
2026-04-03 17:04:07 +0000

    Enhance CustomFormField component with additional input types and props; update PatientForm placeholders for consistency; add new appointment and patient action files.

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -54,7 +54,7 @@ export const PatientForm = () => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
         <section className="mb-12 space-y-4">
           <h1 className="header">Hi there 👋</h1>
-          <p className="text-dark-700">Get started with appointments.</p>
+          <p className="text-dark-750">Get started with appointments.</p>
         </section>
 
         <CustomFormField
@@ -62,7 +62,7 @@ export const PatientForm = () => {
           control={form.control}
           name="name"
           label="Full name"
-          placeholder="John Doe"
+          placeholder="John Woods"
           iconSrc="/assets/icons/user.svg"
           iconAlt="user"
         />
@@ -72,7 +72,7 @@ export const PatientForm = () => {
           control={form.control}
           name="email"
           label="Email"
-          placeholder="johndoe@gmail.com"
+          placeholder="johnwoods@gmail.com"
           iconSrc="/assets/icons/email.svg"
           iconAlt="email"
         />
